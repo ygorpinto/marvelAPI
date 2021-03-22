1a4f61970ae183d438a5318ce381fbe7ce2e0567
Ygor Ferreira <ygor.pinto@hotmail.com>
2021-03-22 01:48:51 -0300

    ok

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,13 @@ import express from 'express'
 import router from './controllers/controller';
 import connectDB from './db/db'
 import cors from 'cors'
+import { OptionsJson } from 'body-parser';
 
 const app = express();
 
 app.use(cors());
 app.use('/',router);
-app.use(express.json({extended: false});
+app.use(express.json({extended: false} as OptionsJson));
 
 connectDB();
 
